186188abc8e1a0141f11777b78f0ea52849f56c8
mcrgnt <microgiant@yandex.ru>
2026-06-29 23:30:59 +0300

    remove dedup, use narrow registry interface; remove topo sort

--- errors.go
+++ errors.go
@@ -2,9 +2,4 @@ package sdi
 
 import "errors"
 
-var (
-	ErrTooManyImplementations = errors.New("too many implementations")
-	ErrAmbiguousDependency    = errors.New("ambiguous dependency")
-	ErrMultipleReplaceable    = errors.New("multiple replaceable defaults")
-	ErrFixedResourceConflict  = errors.New("fixed resource conflict")
-)
+var ErrAmbiguousDependency = errors.New("ambiguous dependency")
